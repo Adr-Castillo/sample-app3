b18dbacb8c2970c6723dc641b5d071b83d3d27e9
Adr-Castillo <343castillo@gmail.com>
2024-10-30 14:01:10 -0400

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   .editorconfig
    	modified:   .env.example
    	modified:   .gitattributes
    	modified:   .gitignore
    	modified:   README.md
    	modified:   app/Http/Controllers/Controller.php
    	modified:   app/Http/Controllers/toDoListController.php
    	modified:   app/Models/User.php
    	modified:   app/Providers/AppServiceProvider.php
    	modified:   artisan
    	modified:   bootstrap/app.php
    	modified:   bootstrap/cache/.gitignore
    	modified:   bootstrap/providers.php
    	modified:   composer.json
    	modified:   composer.lock
    	modified:   config/app.php
    	modified:   config/auth.php
    	modified:   config/cache.php
    	modified:   config/database.php
    	modified:   config/filesystems.php
    	modified:   config/logging.php
    	modified:   config/mail.php
    	modified:   config/queue.php
    	modified:   config/services.php
    	modified:   config/session.php
    	modified:   database/.gitignore
    	modified:   database/factories/UserFactory.php
    	modified:   database/migrations/0001_01_01_000000_create_users_table.php
    	modified:   database/migrations/0001_01_01_000001_create_cache_table.php
    	modified:   database/migrations/0001_01_01_000002_create_jobs_table.php
    	modified:   database/seeders/DatabaseSeeder.php
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   phpunit.xml
    	modified:   postcss.config.js
    	modified:   public/.htaccess
    	modified:   public/favicon.ico
    	modified:   public/images/ProfilePatrick_Star.webp
    	modified:   public/images/vegeta.png
    	modified:   public/index.php
    	modified:   public/robots.txt
    	modified:   resources/App.vue
    	modified:   resources/css/app.css
    	modified:   resources/css/tailwind.css
    	modified:   resources/js/app.js
    	modified:   resources/js/bootstrap.js
    	modified:   resources/js/router.js
    	modified:   resources/js/views/Home.vue
    	modified:   resources/js/views/Profile.vue
    	modified:   resources/js/views/Settings.vue
    	modified:   resources/views/welcome.blade.php
    	modified:   routes/api.php
    	modified:   routes/console.php
    	modified:   routes/web.php
    	modified:   storage/app/.gitignore
    	modified:   storage/app/private/.gitignore
    	modified:   storage/app/public/.gitignore
    	modified:   storage/framework/.gitignore
    	modified:   storage/framework/cache/.gitignore
    	modified:   storage/framework/cache/data/.gitignore
    	modified:   storage/framework/sessions/.gitignore
    	modified:   storage/framework/testing/.gitignore
    	modified:   storage/framework/views/.gitignore
    	modified:   storage/logs/.gitignore
    	modified:   tailwind.config.js
    	modified:   tests/Feature/ExampleTest.php
    	modified:   tests/TestCase.php
    	modified:   tests/Unit/ExampleTest.php
    	modified:   vite.config.js

--- resources/js/views/Profile.vue
+++ resources/js/views/Profile.vue
@@ -1,36 +1,94 @@
 <template>
-    <h1 class="bg-red-700 border-b-8 border-black p-6">
-        <div class="flex justify-between">
-            <div class="bg-green-600 font-extrabold">Welcome to the first iteration of MIC.com</div>
-            <div class="text-white text-center">MIC.COM</div>
-            <div class="Profile flex">
-                <div class="text-white text-center p-2">Logged in as:</div>
-                <img class="rounded-full" src="/public/images/vegeta.png" width="45px" alt="profilePic"/>
+    <!-- nav bar -->
+    <div
+        class="w-full h-[100px] bg-white grid grid-cols-3 items-center px-6 border-b-[1px] border-black/10"
+    >
+        <!-- left side | logo and routes -->
+        <div class="col-span-2 w-full h-fit flex gap-24 items-center">
+            <!-- logo -->
+            <div
+                class="!w-[50px] !h-[50px] relative overflow-hidden justify-items-center"
+            >
+                <img
+                    style="height: 50px !important"
+                    src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/9d322810-86b7-4833-872b-3f382ea26c5e/d7uf4zk-b5e325d9-a245-4932-8c39-f01187e2863c.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzlkMzIyODEwLTg2YjctNDgzMy04NzJiLTNmMzgyZWEyNmM1ZVwvZDd1ZjR6ay1iNWUzMjVkOS1hMjQ1LTQ5MzItOGMzOS1mMDExODdlMjg2M2MucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.Pp768uGpCNbwaLL-nvL11tquuuY_7djqh-8g302qEL0"
+                    alt="Logo"
+                />
             </div>
-        </div>
-    </h1>
-    <body>
-        <div class="min-h-screen bg-gradient-to-b from-blue-800 via-blue-300 to-yellow-500 p-28">
-            <div>To Do</div>
-            <form action="/api/saveTask" method="post" accept-charset="UTF-8">
-             <!-- {{  csrf_field }} -->
-                <input type="text" placeholder="new Task"/>
-                <button type="submit">Add to List</button>
-            </form>
-            <div class ="border-4 border-black text-center">
-                Hi
-            </div>
-        </div>
-    </body>
-</template>
 
+            <!-- routes -->
+            <div class="w-fit h-fit flex items-center gap-8">
+                <p>Home</p>
+                <p>About</p>
+                <p>Services</p>
+                <p>Community</p>
+            </div>
+        </div>
+
+        <!-- right side | user profile -->
+        <div class="w-full h-fit flex gap-6 items-center justify-end">
+            <p>Logged in as:</p>
+
+            <!-- user profile pic -->
+            <div
+                class="w-[50px] h-[50px] rounded-full relative overflow-hidden"
+            >
+                <img
+                    class="w-full h-full object-cover"
+                    src="/public/images/vegeta.png"
+                    alt="profilePic"
+                />
+            </div>
+        </div>
+    </div>
+
+    <!-- current announcements --><!--eventually could be a carousel-->
+    <div class="bg-red-500/20 px-6 flex flex-col justify-center font-bold">
+        <p class="text-2xl">Welcome to the dashboard, Adrian...</p>
+        <p>This is the 1st iteration of MIC.com :) | We are in the process ofbuilding this website!</p>
+    </div>
+
+    <!-- main dashboard -->
+    <div
+        class="h-screen bg-green-300/15 grid grid-cols-5 px-6 border-green-500 border-2"
+    >
+        <!--left side | introduction SAVED FOR LATER-->
+        <div class="col-span-2 flex flex-col place-content-center items-center">
+            <p>Who is Michael?</p>
+            <p>From those who knew him...</p>
+            <p>Photos()</p>
+            <p>Mission statement:</p>
+        </div>
+
+        <!-- right side | Todo List -->
+        <div class="w-full h-fit col-span-3 bg-black/10">
+            <!--Todo List | input & submission form-->
+            <div class="col-span-3 grid grid-cols-3 items-center gap-4 border-2 border-red-500">
+                <p class="text-xl font-bold p-2">What would you like to do today?</p>
+                <input type="text" v-model="newTask" placeholder="Add a task" />
+                <button @click="addTask">Submit</button>
+            </div>
+
+            <!--Todo List | task list-->
+            <div class="h-[15%] items-center gap-4 border-2 border-red-500">
+                <p>Tasks</p>
+                <p>Sasks</p>
+                <div class="text-xl border-2 border-blue-500 px-2"><p>{{ new_task }}</p> </div>
+                <p>Flasks</p>
+                <p>Basks</p>
+            </div>
+        </div>
+    </div>
+</template>
+    <!-- Create fucntion to handle new_task variable and add it to the task list, as well as reassign new_task to an empty string-->
 <script>
 export default {
     name: "Profile",
+    data() {
+        return {
+            new_task: "",
+        }
+    },
 
-    }
-
-
-
-
+};
 </script>
